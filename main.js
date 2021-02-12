4c92c652d7adde8793e9bf413b8c2fbd7614c03b
zolir18 <rc.drift18@gmail.com>
2021-02-12 12:55:08 +0100

    test

--- main.js
+++ main.js
@@ -3,17 +3,17 @@
 	var formk = document.getElementById("elem");
 	let myForm = document.getElementById("myForm");
 
-	// formk.onclick = function(event) {
-	//     event.preventDefault();
-	//     dfff();
+	formk.onclick = function(event) {
+	    event.preventDefault();
+	    dfff();
 
-	// 	if(check == 4){
-	// 		myForm.submit();
-	//  	}
-	//  	 check = 0;
-	// }
+		if(check == 4){
+			myForm.submit();
+	 	}
+	 	 check = 0;
+	}
 	var order = 0;
-	var inputCheck = document.querySelectorAll("input.input_text");
+	var inputCheck = document.querySelectorAll(".input_text");
 	var textInputCheck = document.querySelectorAll("p.f-feedback__p-text");
 	var maskMail = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/; 
 	var maskText = /[^ ]/;
@@ -24,27 +24,33 @@
 		 for(var i = 0;i < 4;i++){
 		 		if(order == 3){
 			 		if(inputCheck[order].value.match(maskMail)){
+
 			 			inputCheck[order].classList.add("green");
-			 			textInputCheck[order].classList.remove("visit");
+			 			inputCheck[order].classList.add("text_visi");
+			 			textInputCheck[order].classList.add("text_visi");
 			 			check++; 
 				 	}	
 				 	else{
 				 		inputCheck[order].classList.add("red");
-				 		inputCheck[order].classList.remove("green");
+				 		inputCheck[order].classList.remove("text_visi");
 				 		textInputCheck[order].classList.add("visit");
+				 		textInputCheck[order].classList.remove("text_visi");
 				 		check = 0;
 				 	}
 			 	}
 			 	else{
 				 	if(inputCheck[order].value.match(maskText)){
+
 				 		inputCheck[order].classList.add("green");
-				 		textInputCheck[order].classList.remove("visit");
+				 		inputCheck[order].classList.remove("red");
+				 		textInputCheck[order].classList.add("text_visi");
 				 		check++; 
 				 	}
 				 	else{
 				 		inputCheck[order].classList.add("red");
 				 		inputCheck[order].classList.remove("green");
 				 		textInputCheck[order].classList.add("visit");
+				 		textInputCheck[order].classList.remove("text_visi");
 				 		check = 0;
 				 	}
 		 		}
@@ -53,6 +59,4 @@
 	  	}
 	  	return order;
 	}
-	function dfd(){
-			alert("Fff");
-		}
+
