889122df2473989acd497803ecc00feebd79231e
zolir18 <rc.drift18@gmail.com>
2021-02-17 23:02:41 +0100

    popup

--- popup.js
+++ popup.js
@@ -1,56 +1,56 @@
 		
 
-	var formk = document.getElementById("elem_popup");
-	let myForm = document.getElementById("myForm_popup");
+	var formk_popup = document.getElementById("elem_popup");
+	let myForm_popup = document.getElementById("myForm_popup_popup");
 
-	formk.onclick = function(event) {
+	formk_popup.onclick = function(event) {
 	    event.preventDefault();
-	    dfff();
+	    popupShow();
 
 		if(check == 4){
-			myForm.submit();
+			myForm_popup.submit();
 	 	}
 	 	 check = 0;
 	}
 	var order = 0;
-	var inputCheck = document.querySelectorAll(".input_text_popup");
-	var textInputCheck = document.querySelectorAll("p.f-feedback__p-text--popup");
+	var inputCheck_popup = document.querySelectorAll(".input_text_popup");
+	var textinputCheck_popup = document.querySelectorAll("p.f-feedback__p-text--popup");
 	var maskMail = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/; 
 	var maskText = /[^ ]/;
 
 	var check = 0;  
-	function dfff(){
+	function popupShow(){
 		order = 0;
 		 for(var i = 0;i < 4;i++){
 		 		if(order == 3){
-			 		if(inputCheck[order].value.match(maskMail)){
+			 		if(inputCheck_popup[order].value.match(maskMail)){
 
-			 			inputCheck[order].classList.add("green");
-			 			inputCheck[order].classList.add("text_visi");
-			 			textInputCheck[order].classList.add("text_visi");
+			 			inputCheck_popup[order].classList.add("green");
+			 			inputCheck_popup[order].classList.add("text_visi");
+			 			textinputCheck_popup[order].classList.add("text_visi");
 			 			check++; 
 				 	}	
 				 	else{
-				 		inputCheck[order].classList.add("red");
-				 		inputCheck[order].classList.remove("text_visi");
-				 		textInputCheck[order].classList.add("visit");
-				 		textInputCheck[order].classList.remove("text_visi");
+				 		inputCheck_popup[order].classList.add("red");
+				 		inputCheck_popup[order].classList.remove("text_visi");
+				 		textinputCheck_popup[order].classList.add("visit");
+				 		textinputCheck_popup[order].classList.remove("text_visi");
 				 		check = 0;
 				 	}
 			 	}
 			 	else{
-				 	if(inputCheck[order].value.match(maskText)){
+				 	if(inputCheck_popup[order].value.match(maskText)){
 
-				 		inputCheck[order].classList.add("green");
-				 		inputCheck[order].classList.remove("red");
-				 		textInputCheck[order].classList.add("text_visi");
+				 		inputCheck_popup[order].classList.add("green");
+				 		inputCheck_popup[order].classList.remove("red");
+				 		textinputCheck_popup[order].classList.add("text_visi");
 				 		check++; 
 				 	}
 				 	else{
-				 		inputCheck[order].classList.add("red");
-				 		inputCheck[order].classList.remove("green");
-				 		textInputCheck[order].classList.add("visit");
-				 		textInputCheck[order].classList.remove("text_visi");
+				 		inputCheck_popup[order].classList.add("red");
+				 		inputCheck_popup[order].classList.remove("green");
+				 		textinputCheck_popup[order].classList.add("visit");
+				 		textinputCheck_popup[order].classList.remove("text_visi");
 				 		check = 0;
 				 	}
 		 		}
